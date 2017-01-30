3619201982462dd70acca8133b966d9bd06214cb
shradha <shradha.dalal123@gmail.com>
2017-01-30 17:00:37 +0530

    commit2
    30-01-2017

--- src/test/java/com/liqvid/po/BrowserLaunch.java
+++ src/test/java/com/liqvid/po/BrowserLaunch.java
@@ -19,15 +19,33 @@ public class BrowserLaunch {
 		   Thread.sleep(6000);
 		   driver.findElement(By.xpath("//span[contains(text(),'Courses')]")).click();
 		   Thread.sleep(5000);
-		   driver.findElement(By.xpath("//label[contains(text(),'Batch')]/..//option[1][contains(text(),'Class 2 - Section B')]")).click();
-		   driver.findElement(By.xpath("//label[contains(text(),'Course')]/..//option[contains(text(),'Course 2')]")).click();
+//		   driver.findElement(By.xpath("//label[contains(text(),'Batch')]/..//option[1][contains(text(),'Class 2 - Section B')]")).click();
+//		   driver.findElement(By.xpath("//label[contains(text(),'Course')]/..//option[contains(text(),'Course 2')]")).click();
+		   driver.findElement(By.xpath("//label[contains(text(),'Batch')]/..//option[2][contains(text(),'Class 3 - Section G')]")).click();
+		   driver.findElement(By.xpath("//label[contains(text(),'Course')]/..//option[contains(text(),'Course 3')]")).click();
 		   Thread.sleep(5000);
 		   driver.findElement(By.xpath("//input[@value='Start Course']")).click();
 		   Thread.sleep(10000);
 		   driver.findElements(By.tagName("iframe")).size();
 		   driver.switchTo().frame(0); 
 //		   driver.findElement(By.xpath("//a[contains(text(),'Snake Charmer')]")).click();
-		   driver.findElement(By.xpath("//a[contains(text(),'The Snowman')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'The Snowman')]")).click();
+//-----------------------------------------------------course 3------------------------------------------------------------------------------		   
+		   
+//		   driver.findElement(By.xpath("//a[contains(text(),'Golden Feather')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'The Butterfly')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Not Possible')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'The Clever Fox')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'A Wise Farmer')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'The Selfish Crabs')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Smart Dove')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Ship to America')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Freedom')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Friendship')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Count Your Blessings!')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'Wise Judgement')]")).click();
+//		   driver.findElement(By.xpath("//a[contains(text(),'The White Crow')]")).click();
+		   driver.findElement(By.xpath("//a[contains(text(),'Aeroplane Story')]")).click();
 		   Thread.sleep(10000);
 	}
 
